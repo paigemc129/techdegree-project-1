f7f28d9eb8e76b7c270fa2fda358869933576ce4
paigemc129 <90272491+paigemc129@users.noreply.github.com>
2021-09-27 10:10:37 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+const quotes = [
+  {quote: 'To think in terms of either pessimism or optimism oversimplifies the truth. The problem is to see reality as it is.' , source: 'Thích Nhất Hᾳnh'},
+  {quote: 'Everything that has a beginning has an ending. Make your peace with that and all will be well.', source: 'Jack Kornfield'},
+  {quote: 'Nothing every goes away until it has taught us what we need to know.', source: 'Pema Chödrön'},
+  {quote: "Don't believe everything you think. Thoughts are just that - thoughts." , source: 'Allan Lokos'},
+  {quote: 'Looking at beauty in the world, is the first step of purifying the mind.', source: 'Amit Ray'}
+
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+const getRandomQuote 
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
